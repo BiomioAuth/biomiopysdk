1555dfe529d7bef3a427df0ef19fe1880ec6bd60
vitaliusparubochyi <vitalius.parubochyi@vakoms.com.ua>
2017-02-28 12:11:34 +0200

    Added settings for development gate

--- coreapi/settings.py
+++ coreapi/settings.py
@@ -2,6 +2,6 @@ DEFAULT_SOCKET_TIMEOUT = 5  # seconds
 SSL_OPTIONS = {
     "ca_certs": "server.pem"
 }
-WEBSOCKET_HOST = ""
-WEBSOCKET_PORT = 0
+WEBSOCKET_HOST = "gate-dev.biom.io"
+WEBSOCKET_PORT = "8080"
 WEBSOCKET_URL = "wss://{host}:{port}/websocket".format(host=WEBSOCKET_HOST, port=WEBSOCKET_PORT)
