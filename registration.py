473372e87fda5d0e382439729b6cfdf5331c5656
Vitalius Parubochyi <vitalius.parubochyi@vakoms.com.ua>
2017-02-28 09:01:57 -0800

    Fixed registration script

--- registration.py
+++ registration.py
@@ -7,7 +7,7 @@ if __name__ == "__main__":
     os_id = "Android_6.0.1"
     network_type = "WIFI"
     api = BiomioMessagingAPI(app_type=app_type, dev_id=dev_id, os_id=os_id)
-    response = api.hello({'secret': ""})
-    print response
+    response = api.hello(**{'secret': "68957483"})
+    print dict(response)
     if response.header.oid == 'serverHello':
         api.ack()
