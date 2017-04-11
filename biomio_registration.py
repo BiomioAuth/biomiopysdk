f82c958fcdb82182bf20153356822a8d3d67d9b3
Vitalius <vitalius.parubochyi@vakoms.com.ua>
2017-04-11 04:15:17 -0700

    Fixed BiomioMessagingAPI initialization

--- biomio_registration.py
+++ biomio_registration.py
@@ -7,7 +7,7 @@ class BiomioRegistration(object):
         self._port = port
 
     def register(self, secret, app_type, os_id='', dev_id=''):
-        api = BiomioMessagingAPI(host=self._host, port=self._port, app_type=app_type,
+        api = BiomioMessagingAPI(host=self._host, port=self._port, app_type=app_type, app_id='',
                                  dev_id=dev_id, os_id=os_id)
         response = api.hello(**{'secret': secret})
         if response and response.msg.oid == 'serverHello':
